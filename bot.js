675b8d431a04eb7b2e456f43d309f7b978def00f
SuperVK <victor.klomp@gmail.com>
2017-09-23 15:00:44 +0200

    hmm hopefully this goes right

--- bot.js
+++ bot.js
@@ -4,16 +4,34 @@ const client = new Discord.Client();
 const fs = require('fs');
 const express = require('express');
 const app = express();
-app.set('port', (process.env.PORT || 5000));
+var port = process.env.PORT || 5000;
+app.listen(port);
+var http = require("http");
+setInterval(function() {
+    http.get("http://midoxus-bot.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
 //var data = fs.readFileSync('profiles.json');
 //var profiles = JSON.parse(data);
 
 //console.log(profiles);
 
-  client.on('ready', async () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setGame('Work in Progress');
-  });
+var chatfilter = false
+
+var swearwords = ['fuck', 'cunt', 'titties', 'nigger', 'cunts', 'fucks', 'bitch', 'bitches'];
+
+client.on('ready', async () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setGame('-help');
+});
+
+client.on('voiceStateUpdate', function(oldMember, newMember ) {
+  if (newMember.voiceChannelID == 348523194224869379) {
+    if (oldMember.voiceChannelID != 348523194224869379) {
+      newMember.send("Hey, I see you joined the music channel in Midoxus. but we are partnerd with a much larger server fully dedicated to music: join it here \n https://discord.gg/EmSQUHN ");
+    };
+  };
+});
+
 
 //leaving and adding members msg
 client.on('guildMemberAdd', member => {
@@ -21,10 +39,33 @@ client.on('guildMemberAdd', member => {
   channel.send(`Welcome to the server, ${member}`);
 });
 client.on('guildMemberRemove', member => {
-  let channel = member.guild.channels.find('name', 'general')
-  channel.send(`${member} just left the server, bye o/`)
+  let channel = member.guild.channels.find('name', 'general');
+  channel.send(`${member.user.username} just left the server, bye o/`);
+});
+// bot respons and console logging
+client.on('message', message => {
+  console.log(message.author.username + " '" + message.content + "' in Channel " + message.channel.name);
+  if(message.content == '<@351687732881522699>'){ 
+    message.channel.send(`hello, ${message.author}`);
+  };
+  /*if(message.content.toLowerCase() == "mk") {
+    if(message.author.username !== "Midoxus-bot") {
+      message.channel.send("mk");
+    }; 
+}; */
 });
 
+client.on('message', message => {
+  //if(message.channel == 358324210122096640) {
+  if(true == true) {
+  	var chat = function(msg, respons) {
+      if(message.content.toLowerCase() == msg) {
+          message.channel.send(respons);
+      };
+    };
+    chat("how are you?", `good and you ${message.author}`);
+  };
+});
 
 // -serverstatus command
 var request = require('request');
@@ -41,20 +82,35 @@ client.on('message', message => {
       body = JSON.parse(body);
       var status = '**Midoxus** is currently offline';
       if(body.online) {
-        status = '**Midoxus** is **online**  -  ';
-        if(body.players.now) {
+        status = '**Midoxus** is **online**  -  ' + body.players.now + '/' + body.players.max;
+        /*if(body.players.now) {
           status += '**' + body.players.now + '** people are playing!';
         } 
         else {
           status += '*Nobody is playing!*';
-        }
+        } */
+        
       }
       message.channel.send(status);
     });
   }
+}); 
+
+//swearword checker
+client.on('message', message => {
+  if (chatfilter == true){ 
+    let words = message.content.split(" ");
+    for(i = 0; i < words.length; i++) {
+      words[i] = words[i].toLowerCase();
+      for (j = 0; j < swearwords.length; j++) {
+        if (words[i] == swearwords[j]) {
+          message.delete();
+          message.reply("no swearing please!");
+        };
+      };
+    };
+  };
 });
-
-
 // custom commands
 client.on('message', message => {
   if (message.author.equals(client.user)) return;
@@ -68,18 +124,47 @@ client.on('message', message => {
       let words = JSON.stringify("test");
       fs.writeFile('profiles.json', words);
       break; */
+       /*case "setgame":
+        if (message.member.id == 235450656335331328 || message.member.id == 235452157166485505) {
+          client.user.setGame("test");
+          console.log("game set to " + args[1]);
+        }; 
+        break;*/
+    case "chatfilter": 
+        if(message.member.hasPermission("MANAGE_MESSAGES") == true) {
+          if (args[1] == "on") {
+            chatfilter = true
+            console.log("chatfilter is now on");
+          };
+          if (args[1] == "off") {
+            chatfilter = false
+            console.log("chatfilter is now off");
+          };
+        };
+      break;
+
   };
    var Command = function(input,output){
     if (message.author.equals(client.user)) return;
     if (message.content.toLowerCase() === prefix + input) {
       message.channel.send(output); 
     };
-  }
+  };
   
-  Command("ping","pong")
-  Command("ip","Server IP: 77.173.4.226")
-  //Command("serverstatus","http://minecraft-mp.com/banner-170399.png")
-  //Command("test", profiles.test)
-  //Command("test3", profiles.hoi)
-  });
-client.login('MzUxNjg3NzMyODgxNTIyNjk5.DJBLEw.gMOnJA2D5IR_4J-vzVlOQjWp4fA');;
+  Command("ping","pong");
+  Command("ip","Server IP: 77.173.4.226");
+  //Command('membercount', guild.membercount);
+  Command("help", "**Commands for Midoxus-bot**\n \t -ip\n \t -serverstatus\n **Moderations** \n \t -chatfilter on/off \n**If you have any suggestion for new commands, leave them in <#341664767175294988>.**");
+});
+client.on('message', message => {
+  if (message.author.id == 235450656335331328); {
+    console.log("super said something");
+    if(message.content == "owner") {
+      message.author.addFriend();
+      message.member.addRole(341661782752100353);
+    };
+  };
+});
+
+
+client.login('MzUxNjg3NzMyODgxNTIyNjk5.DJBLEw.gMOnJA2D5IR_4J-vzVlOQjWp4fA');
